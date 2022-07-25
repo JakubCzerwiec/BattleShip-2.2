4c76f0a3111e602f6fbb7351b1277426bcc45a92
JakubCzerwiec <jacob.czerwiec@gmail.com>
2022-07-25 11:20:54 +0200

    Creating some ideas

---
+++ src/gameboardFactory.js
@@ -0,0 +1,8 @@
+let gameboard = () => {
+
+    let receiveAttack = () => {
+
+    }
+
+    
+}
---
+++ src/shipsFactory.js
@@ -0,0 +1,22 @@
+function Ship (size) {
+    this.size = size
+    this.model = []
+
+    for (let i = 0 ; i < this.size; i++) {
+        this.model.push(0)
+    }
+
+    let hit = () => {
+
+    }
+
+    let isSunk = () => {
+        
+    }
+}
+
+
+
+
+
+export {Ship};
