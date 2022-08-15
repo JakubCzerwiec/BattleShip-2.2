e80349bc5d70b0e7ffb7bbea7e2f72e590d6c993
JakubCzerwiec <jacob.czerwiec@gmail.com>
2022-08-15 11:06:24 +0200

    Form for choosing ships orientation, beggining

--- src/script.js
+++ src/script.js
@@ -16,6 +16,29 @@ gameboardFactory(100);
 
 renderBoard();
 
+let shipsForm = document.querySelector('.shipsForm');
+
+let body = document.querySelector('body')
+shipsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let horisonRadio = document.getElementById('horisonRadio').checked;
+    let verticalRadio = document.getElementById('verticalRadio').checked;
+    console.log(horisonRadio)
+    if (horisonRadio == true) {
+      //  listenerForPlacingShipsHorisontal()
+        body.className = ('red')
+    }
+
+    else if (verticalRadio == true) {
+      //  listenerForPlacingShipsVertical()
+        body.className = ('green')
+    }
+    
+    // when click, toggle class and call different listener
+    // do it with input, after validation -> lunch listener for placing ships
+    // logic for calling different listener when placing ship on gameboard
+})
+
 shipsSetup();
 
 let gameplay = document.querySelector('.gameplay');
--- src/shipsOnBoard.js
+++ src/shipsOnBoard.js
@@ -2,45 +2,47 @@ import { game } from './gameboardFactory';
 import {Ship} from './shipsFactory';
 import {renderBoard} from './renderBoard'
 
+import {listenerForPlacingShipsVertical} from './listenerForPlacingShipsVertical';
+import {listenerForPlacingShipsHorisontal} from './listenerForPlacingShipsHorisontal';
+
 let main = document.querySelector('main');
 
-let shipsInHarbour = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+let shipsInHarbour = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
+let shipsForm = document.querySelector('.shipsForm');
+
+let body = document.querySelector('body')
 
 function shipsSetup () {
-    let shipsBox = document.createElement('div');
-    shipsBox.className = 'shipsBox';
 
 
-    // Box to pop up before placing ship to tell size and ask about orientation
-    main.appendChild(shipsBox);
+    shipsForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // let horisonRadio = document.getElementById('horisonRadio').value;
+        // let verticalRadio = document.getElementById('verticalRadio').value;
+        if (shipsForm.value == 'horison') {
+            listenerForPlacingShipsHorisontal()
+            body.className = ('red')
+        }
 
-    let shipName = document.createElement('div');
-    shipName.className = 'shipName';
-    shipsBox.appendChild(shipName);
-    shipsBox.innerText = `Now let's put ${shipsInHarbour[0]} on the gameboard!`;
-
-    let orientation = document.createElement('div');
-    orientation.className = 'orientation';
-    shipsBox.appendChild(orientation);
-    orientation.innerText = 'Orientation of the ship?';
-
-    let orientationSwitch = document.createElement('div');
-    orientationSwitch.className = 'orientationSwitchH';
-    orientation.appendChild(orientationSwitch);
-
-    orientationSwitch.addEventListener('click', () => {
+        else if (shipsForm.value == 'vertical') {
+            listenerForPlacingShipsVertical()
+            body.className = ('green')
+        }
+        
         // when click, toggle class and call different listener
+        // do it with input, after validation -> lunch listener for placing ships
         // logic for calling different listener when placing ship on gameboard
     })
 
-    let allSquers = document.querySelectorAll('.squere');
+    // let allSquers = document.querySelectorAll('.squere');
 
-    for (let i = 0; i < allSquers.length; i++) {
-        allSquers[i].addEventListener('click', () => {
-            game[i].shipOn == true;
-            renderBoard()
-        })
-    }
+    // for (let i = 0; i < allSquers.length; i++) {
+    //     allSquers[i].addEventListener('click', () => {
+    //         game[i].shipOn == true;
+    //         renderBoard()
+    //     })
+    // }
 
 
 }
